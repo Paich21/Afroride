ed1511ca23d40656d5abc9bcbc87f83e7efd90e7
Paich21 <94931557+Paich21@users.noreply.github.com>
2023-06-03 00:01:47 +0100

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,20 +1,28 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-  // Retrieve form data
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Get the form data
   $pickupLocation = $_POST["pickup-location"];
   $destination = $_POST["destination"];
   $phoneNumber = $_POST["phone-number"];
 
-  // Compose the email
-  $to = "blaysam2021@gmail.com";
+  // Set up the email
+  $to = "paichbley@gmail.com";
   $subject = "New Booking!!";
   $message = "Here is the booking:\n\n";
   $message .= "Pickup Location: " . $pickupLocation . "\n";
   $message .= "Destination: " . $destination . "\n";
-  $message .= "Phone Number: " . $phoneNumber . "\n";
+  $message .= "Phone Number: " . $phoneNumber;
 
   // Send the email
-  $headers = "From: Your Website <noreply@example.com>";
-  mail($to, $subject, $message, $headers);
+  $headers = "From: Your Name <your-email@example.com>"; // Replace with your email address
+  if (mail($to, $subject, $message, $headers)) {
+    // Email sent successfully
+    header("Location: index.html?status=success");
+    exit();
+  } else {
+    // Email failed to send
+    header("Location: index.html?status=error");
+    exit();
+  }
 }
 ?>
